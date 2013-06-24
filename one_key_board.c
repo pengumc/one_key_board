5346a32ea63de753176ffb6d53690be57b1d2376
Pengu MC @ vito <michiel.van.der.coelen@gmail.com>
2013-06-24 23:13:36 +0200

    it was working all along
    apparently windows doesn't like you changing the descriptor each time
    you connect. and it freezes and refuses the device. just uninstall it in
    de devmgmt after the descriptor has changed

--- one_key_board.c
+++ one_key_board.c
@@ -9,6 +9,7 @@
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
+#include <util/delay.h> 
 
 #include "usbdrv.h"
 #include "oddebug.h"
@@ -22,25 +23,23 @@ static uchar    reportBuffer[1];    /* buffer for HID reports */
 static uchar    idleRate;           /* in 4 ms units */
 
 
-const PROGMEM char usbHidReportDescriptor[35] = {   /* USB report descriptor */
-    0x05, 0x0c,                    // USAGE_PAGE (Consumer Devices)
-    0x09, 0x01,                    // USAGE (Consumer Control)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x09, 0xe0,                    //   USAGE (Volume)
-    0x15, 0xff,                    //   LOGICAL_MINIMUM (-1)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x02,                    //   REPORT_SIZE (2)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x81, 0x06,                    //   INPUT (Data,Var,Rel)
-    0x09, 0xe2,                    //   USAGE (Mute)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x81, 0x06,                    //   INPUT (Data,Var,Rel)
-    0x95, 0x05,                    //   REPORT_COUNT (5)
-    0x81, 0x07,                    //   INPUT (Cnst,Var,Rel)
-    0xc0                           // END_COLLECTION
+const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
+    0x05, 0x0C, // Usage Page (Consumer Devices)
+    0x09, 0x01, // Usage (Consumer Control)
+    0xA1, 0x01, // Collection (Application)
+        0x15, 0x00, //Logical Minimum (0)
+        0x25, 0x01, //Logical Maximum (1)
+        0x09, 0xE9, //Usage (Volume Up)
+        0x09, 0xEA, //Usage (Volume Down)
+        0x75, 0x01, //Report Size (1)
+        0x95, 0x02, //Report Count (2)
+        0x81, 0x02, //Input (Data, Variable, Absolute)
+        0x09, 0xE2, //Usage (Mute)
+        0x95, 0x01, //Report Count (1)
+        0x81, 0x06, //Input (Data, Variable, Relative)
+        0x95, 0x05, //Report Count (5)
+        0x81, 0x07, //Input (Constant)
+    0xC0 // End Collection
 };
 
  
@@ -70,16 +69,29 @@ uchar keydown = 0;
 uchar prev = 0;
 uchar changed = 0;
 int main(){
-    usbInit();
-    sei();
     reportBuffer[0] = 0x00;//(1<<1);
+    wdt_enable(WDTO_1S);
+    usbInit();
+    usbDeviceDisconnect();
+    register uint8_t i=0;
+    while(--i){
+        wdt_reset();
+        _delay_ms(1);
+    }
+    usbDeviceConnect();
+    
+    sei();
+
     CLR(DDRB, PB0);
     SET(DDRD, PD7);
     TCCR0B = 5; //prescale TC0 1024, 20 MHz -> 50 ns * 1024 * 255 = 13.056 ms
+
     while(1){
         usbPoll();
+        wdt_reset();
         if(CHK(TIFR0, TOV0)){
             TIFR0 = 1<<TOV0;
+            TCNT0 = 0;
             if(CHK(PINB, PB0) && changed == 0){
                 keydown = 1;
                 if (prev == keydown) changed = 0;
@@ -97,11 +109,12 @@ int main(){
             TIFR0 = 1<<TOV0;
             TCNT0 = 0;
             if(keydown){
-                reportBuffer[0] = 0x04;
+                reportBuffer[0] = 0x02;
             }else{
                 reportBuffer[0] = 0x00;
             }
             TOG(PORTD, PD7);
+            wdt_reset();
             usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
         }
     }
